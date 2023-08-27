25815dfa6d6e3fd914873d264699ef52a12045a0
Padmanabha Reddy Yatham <padmanabha2516@gmail.com>
2023-08-27 20:02:07 +0530

    revert the commit_miss mathes

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,72 +1,18 @@
-import datetime
-from typing import Optional
+def solve(heights):
+    stk = [-1]
+    heights.append(0)
+    ans = [0]
+
+    for i in range(len(heights)):
+        while heights[i] < heights[stk[-1]]:
+            h = heights[stk.pop()]
+            width = i - stk[-1] - 1
+            ans.append(h * width)
+        stk.append(i)
+    return ans
 
 
-class CustomisedError(Exception):
-    """
-    Customised exception for SavedSnippets.
-    """
+nums = list(map(int, input().split()))
 
 
-class SavedSnippets:
-    def __init__(self):
-        self.today: Optional[datetime.datetime] = datetime.datetime.today()
-        self.count: Optional[int] = None
-        self.sum: Optional[int] = None
-
-    # region properties
-    @property
-    def count(self) -> int:
-        """
-        Gets the count value.
-        """
-        if self.count is None:
-            raise CustomisedError("count is not intitialized")
-        return self.count
-
-    @count.setter
-    def count(self, value: int) -> None:
-        self.count = value
-
-    @property
-    def sum(self) -> int:
-        """
-        Gets the sum value.
-        """
-        if self.sum is None:
-            raise CustomisedError("sum is not intitialized")
-        return self.sum
-
-    @sum.setter
-    def sum(self, value: int) -> None:
-        self.sum = value
-
-    # endregion
-
-    # region code snippets
-    def check_given_number_is_armstorng(self, num: int) -> bool:
-        """
-        Verifies the given number is a valid armstrong number or not.
-
-        Args:
-            num (int): input number value
-
-        Returns:
-            bool: status of the number is armstrong or not.
-        """
-        temp_num = num
-        self.sum = 0
-
-        while temp_num > 0:
-            c = temp_num % 10
-            self.sum += c**3
-            temp_num //= 10
-
-        return self.sum == num
-
-    # endregion
-
-
-if __name__ == "__main__":
-    snippets = SavedSnippets()
-    snippets.check_given_number_is_armstorng(65325)
+print(max(solve(nums)))
